552db53de2428f6a67bb4bfc85fbee90a9db11b2
Parker Grant <parkergrant@parkers-mbp.uconnect.utah.edu>
2018-06-05 21:22:31 -0600

    updated dropdown functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -177,19 +177,29 @@ $(document).ready(function () {
     });
     //$('.collapsible').collapsible();
     $('.collapsible').collapsible();
+
+    $('select').formSelect();
 });
+
 
 // Fandango API
 
-var fandangoApiKey = "35cy3hs5nebuedxktkaebzzr"
-var userZipCode = "84003";
-var fandangoQueryURL = "http://api.fandango.com/v1/?op=theatersbypostalcodesearch&postalcode=" + userZipCode + "&apikey=" + fandangoApiKey
+// function movieResponse() {
+//     ;
 
-$.ajax({
-    URL: fandangoQueryURL,
-    Method: "GET",
-}).then(function (response) {
+//     var fandangoApiKey = "35cy3hs5nebuedxktkaebzzr";
+//     var userZipCode = "84003";
+//     var sharedSecret = "2UQChprwaT";
+//     var fandangoQueryURL = "http://api.fandango.com/v1/?op=theatersbypostalcodesearch&postalcode=" + userZipCode + "&apikey=" + fandangoApiKey;
 
-    console.log(response)
+//     $.ajax({
+//         URL: fandangoQueryURL,
+//         Method: "GET",
+//     }).then(function (response) {
 
-});
+//         console.log(response)
+
+//     });
+
+// }
+// movieResponse();
